65bd1da54ab34d2c5fe6de01fb491abeea4fec5a
Danny <tranduynhatanh1708@gmail.com>
2024-04-19 12:59:46 +0700

    second

--- sample_tflite.py
+++ sample_tflite.py
@@ -2,11 +2,9 @@ from config.Yolov9 import Yolov9TFlite
 import cv2 
 
 
-image = cv2.imread("/home/danny/Honda/YOLOX-ONNX-TFLite-Sample/representative_dataset/000000000139.jpg")
+image = cv2.imread("/home/../../yolox/representative_dataset/000000000139.jpg")
 
 
-
-
-yolov9 = Yolov9TFlite(model_path='/home/danny/Honda/Yolov9-tflite/yolov9_e_float16_quantize.tflite')
+yolov9 = Yolov9TFlite()
 
 yolov9.inference(image)
