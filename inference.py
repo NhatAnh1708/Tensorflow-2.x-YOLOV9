a95fab43f3a42e2460d132842e83a1fb6ec7c932
Danny <tranduynhatanh1708@gmail.com>
2024-04-22 16:50:34 +0700

    dev-it

--- inference.py
+++ inference.py
@@ -1,4 +1,6 @@
 import argparse
+import logging
+import os
 import time
 
 import cv2
@@ -7,78 +9,161 @@ import numpy as np
 from config.config import load_config
 from src.utils import *
 
-config = load_config("config/model/yolov9.yaml")
-## Load configuration from a YAML file.
-INPUT_SHAPE = config["input_shape"]
-CONF_THRESHOLD = config["conf_threshold"]
-MODEL_PATH = config["model_path"]
-COCO_CLASSES = config["coco_classes"]
 
-VIDEO_PATH = "data_test/testcase1.mp4"
-cap = cv2.VideoCapture(VIDEO_PATH)
-labels = load_labels(path=COCO_CLASSES)
-while True:
-    ret, frame = cap.read()
-    if ret:
+def get_args():
+    """Get command-line arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="config/model/Yolov9e/yolov9_416.yaml")
+    parser.add_argument("--model", type=str, default="yolov9")
+    parser.add_argument("--video", type=str, default="data_test/testcase8.mp4")
+    args = parser.parse_args()
+
+    return args
+
+
+def run_yolov9(
+    frame, input_shape, prediction_shape, interpreter, input_details, output_details
+):
+    """YOLO v9"""
+    bgr, ratio, dwdh = letterbox(frame, (input_shape, input_shape))
+    rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+    tensor = blob(rgb)
+    tensor = np.ascontiguousarray(tensor)
+    interpreter.set_tensor(input_details[0]["index"], tensor)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]["index"])
+    predictions = np.array(predictions).reshape((84, prediction_shape))
+    predictions = predictions.T
+    return predictions
+
+
+def run_yolov8(
+    frame, input_shape, prediction_shape, interpreter, input_details, output_details
+):
+    """YOLO v8"""
+    bgr, _, _ = letterbox(frame, input_shape)
+    rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+    tensor = blob(rgb)
+    tensor = np.transpose(tensor, (0, 2, 3, 1))
+    tensor = np.ascontiguousarray(tensor)
+    interpreter.set_tensor(input_details[0]['index'], tensor)
+    interpreter.invoke()
+    predictions = interpreter.get_tensor(output_details[0]['index'])
+    predictions = np.array(predictions).reshape((84, prediction_shape))
+
+    predictions = predictions.T
+    return predictions
+
+
+def main():
+    """Run"""
+    arg = get_args()
+    config_path = arg.config
+    config = load_config(config_path)
+    ## Load configuration from a YAML file.
+    input_shape = config["input_shape"]
+    conf_threshold = config["conf_threshold"]
+    model_path = config["model_path"]
+    coco_classes = config["coco_classes"]
+    prediction_shape = config["prediction_shape"]
+    interpreter, input_details, output_details = load_model_tflite(
+        model_path, num_threads=10
+    )
+    count = 0
+    video_path = arg.video
+    if video_path == "0":
+        video_path = 0
+        video_name = "webcam"
+    else:
+        video_name = video_path.split("/")[-1].split(".")[0]
+    model_name = model_path.split("/")[-1].split(".")[0]
+
+    cap = cv2.VideoCapture(video_path)
+
+    if not os.path.exists(f"output_tflite/{model_name}/{video_name}"):
+        os.makedirs(f"output_tflite/{model_name}/{video_name}")
+    labels = load_labels(path=coco_classes)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
         start = time.time()
-        interpreter, input_details, output_details = load_model_tflite(
-            MODEL_PATH, num_threads=10
-        )
-        frame = cv2.resize(frame, (INPUT_SHAPE, INPUT_SHAPE))
-        bgr, ratio, dwdh = letterbox(frame, (INPUT_SHAPE, INPUT_SHAPE))
-        rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
-        tensor = blob(rgb)
-        tensor = np.ascontiguousarray(tensor)
-        interpreter.set_tensor(input_details[0]["index"], tensor)
-        interpreter.invoke()
-        predictions = interpreter.get_tensor(output_details[0]["index"])
-        predictions = np.array(predictions).reshape((84, 8400))
-        predictions = predictions.T
-
+        frame = cv2.resize(frame, (input_shape, input_shape))
+        if arg.model == "yolov9":
+            logging.info("Running YOLO v9")
+            predictions = run_yolov9(
+                frame,
+                input_shape,
+                prediction_shape,
+                interpreter,
+                input_details,
+                output_details,
+            )
+        elif arg.model == "yolov8":
+            logging.info("Running YOLO v8")
+            predictions = run_yolov8(
+                frame,
+                input_shape,
+                prediction_shape,
+                interpreter,
+                input_details,
+                output_details,
+            )
         scores = np.max(predictions[:, 4:], axis=1)
-        predictions = predictions[scores > CONF_THRESHOLD, :]
-        scores = scores[scores > CONF_THRESHOLD]
+        predictions = predictions[scores > conf_threshold, :]
+        scores = scores[scores > conf_threshold]
         class_ids = np.argmax(predictions[:, 4:], axis=1)
         boxes = predictions[:, :4]
-        indices = nms(boxes, scores, CONF_THRESHOLD)
+        indices = nms(boxes, scores, conf_threshold)
+        list_bboxes = []
         for bbox, score, label in zip(
             xywh2xyxy(boxes[indices]), scores[indices], class_ids[indices]
         ):
-            x, y, x2, y2 = bbox
-            x1 = int(x)
-            y1 = int(y)
-            x2 = int(x2)
-            y2 = int(y2)
+            bbox = scale_bbox(bbox, arg.model, input_shape)
             cls_id = int(label)
-            cls = labels[cls_id]
-            if labels[label] == "person" or labels[label] == "car":
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            # cls = labels[cls_id]
+            if cls_id == 0:
+                list_bboxes.append(bbox)
+                color = (0, 0, 128)
+                draw_bbox(image=frame, bbox=bbox, color=color)
+            if cls_id == 2:
+                list_bboxes.append(bbox)
+                color = (0, 128, 0)
+                draw_bbox(image=frame, bbox=bbox, color=color)
             else:
                 continue
-            cv2.putText(
-                frame,
-                labels[label],
-                (x1, y1),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0, 255, 0),
-                2,
-            )
-        fps = 1 / (time.time() - start)
+        latency = (time.time() - start) * 1000
         cv2.putText(
             frame,
-            f"FPS: {fps:.2f}",
+            f"Bboxes: {len(list_bboxes)}",
+            (10, 100),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 0),
+            2,
+        )
+        cv2.putText(
+            frame,
+            f"Latency: {latency:.2f} ms",
             (10, 50),
             cv2.FONT_HERSHEY_SIMPLEX,
             1,
-            (0, 255, 0),
+            (0, 0, 0),
             2,
         )
-        cv2.imwrite("assets/output1.jpg", frame)
+        if count % 20 == 0 and count < 1000:
+            logging.warning("Loading frame")
+            cv2.imwrite(
+                f"output_tflite/{model_name}/{video_name}/frame_{str(count)}.jpg",
+                frame,
+            )
+        count += 1
         cv2.imshow("video", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-    else:
-        break
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
--- src/export_tflite.py
+++ src/export_tflite.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_saved_model("yolov9e_pb")
-converter.optimizations = [tf.lite.Optimize.DEFAULT]
-tflite_quantized_model = converter.convert()
-with open("weights/yolov9_e_float32.tflite", "wb") as path:
-    path.write(tflite_quantized_model)
-
 # converter = tf.lite.TFLiteConverter.from_saved_model("yolov9e_pb")
 # converter.optimizations = [tf.lite.Optimize.DEFAULT]
-# converter.target_spec.supported_types = [tf.float16]
 # tflite_quantized_model = converter.convert()
-# open("yolox_nano_float16_quantize.tflite", "wb").write(tflite_quantized_model)
+# with open("weights/yolov9_e_float32.tflite", "wb") as path:
+#     path.write(tflite_quantized_model)
+
+converter = tf.lite.TFLiteConverter.from_saved_model("yolov9e_pb")
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.target_spec.supported_types = [tf.float16]
+tflite_quantized_model = converter.convert()
+with open("weights/yolov9_e_float16_416.tflite", "wb") as path:
+    path.write(tflite_quantized_model)
